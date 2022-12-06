49d4d462fd88a20b27af073b40c739dacf02b692
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2022-12-06 12:09:01 -0300

    Initial steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-forms-validation';
+  user: User = new User("", "");
+
+  onSubmitTemplateBased() {
+    console.log("Template has been submited!")
+  }
 }
